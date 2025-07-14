3272f1679508d85c190da065458ae01cc1ef8107
badalkr2004 <badalkr279@gmail.com>
2025-07-14 13:41:11 +0530

    fix: env import

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL =
+  import.meta.env.API_BASE_URL || "http://localhost:5000/api";
 export const CLOUD_NAME = "dlyfwiaon";
