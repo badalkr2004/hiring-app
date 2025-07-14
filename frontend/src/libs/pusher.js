9aa3a5aa388c8f7e25726775779461d0d7bc84af
badalkr2004 <badalkr279@gmail.com>
2025-07-14 14:44:46 +0530

    fix push auth url

--- frontend/src/libs/pusher.js
+++ frontend/src/libs/pusher.js
@@ -1,12 +1,13 @@
 import Pusher from "pusher-js";
+import { API_BASE_URL } from "../config/api";
 
 let pusherInstance = null;
-
+API_BASE_URL;
 export const initializePusher = () => {
   if (!pusherInstance) {
     pusherInstance = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
       cluster: import.meta.env.VITE_PUSHER_CLUSTER,
-      authEndpoint: "http://localhost:5000/api/messages/pusher/auth",
+      authEndpoint: `${API_BASE_URL}/messages/pusher/auth`,
       auth: {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
